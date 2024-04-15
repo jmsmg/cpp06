2920519cb29db5c1e2721f3465465845c9e42702
Seonggon, Cho <77792994+jmsmg@users.noreply.github.com>
2024-04-15 07:19:26 +0000

    ok

---
+++ ex02/Base.cpp
@@ -0,0 +1,38 @@
+#include "Base.hpp"
+
+Base::Base()
+{
+	
+}
+Base::~Base()
+{
+
+}
+
+Base	*generate(void)
+{
+	std::srand(std::time(NULL));
+	int r = rand() % 3;
+	if (r == 0)
+	{
+		return ()
+	}
+	else if (r == 1)
+	{
+
+	}
+	else
+	{
+
+	}
+}
+
+void	identify(Base *p)
+{
+
+}
+
+void	identify(Base &p)
+{
+
+}
